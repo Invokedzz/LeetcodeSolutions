a3bb31f6df7d220e280fafe864e5c774cd339173
Samuel Nóbrega <samuelnobrega902@gmail.com>
2025-01-14 18:21:26 -0300

    ScoreOfAString 1th resolution

--- src/ScoreOfAString/ScoreOfAString.java
+++ src/ScoreOfAString/ScoreOfAString.java
@@ -1,4 +1,27 @@
 package ScoreOfAString;
 
 public class ScoreOfAString {
+
+    public static void main ( String[] args ) {
+
+        int result = scoreString( "hello");
+
+        System.out.println( result );
+
+    }
+
+    private static int scoreString ( String element ) {
+
+        int score = 0;
+
+        for ( int index = 0; index < element.length() - 1; index++ ) {
+
+            score += Math.abs(element.charAt( index ) - element.charAt( index + 1 ));
+
+        }
+
+        return score;
+
+    }
+
 }
